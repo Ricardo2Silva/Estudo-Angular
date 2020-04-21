70adbf6135d336024e1a8367c8960b93d6f3b20b
Ricardo <ricardojonasdasilva60@gmail.com>
2020-04-21 18:57:52 -0300

    input-ngfor

--- src/app/lista-compras/lista-compras.module.ts
+++ src/app/lista-compras/lista-compras.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 //componentes
 import {ListaComprasContainerComponent} from './lista-compras-container/lista-compras-container.component'
 import {ListaComprasContadorComponent} from './lista-compras-contador/lista-compras-contador.component'
-import {ListaComprasListagemComponent} from './lista-compras-listagem/lista-compras-listagem.component'
+import {ListaComprasItemComponent} from './lista-compras-item/lista-compras-item.component'
 
 
 
@@ -13,7 +13,7 @@ import {ListaComprasListagemComponent} from './lista-compras-listagem/lista-comp
   declarations:[
     ListaComprasContainerComponent,
     ListaComprasContadorComponent,
-    ListaComprasListagemComponent
+    ListaComprasItemComponent
   ],
   imports:[
     CommonModule
